ab65805b279a32132219872c735677fda3d72f26
consonganga <102622401+consonganga@users.noreply.github.com>
2025-06-06 16:29:11 +0300

    Update app.R

--- app.R
+++ app.R
@@ -10,32 +10,6 @@ library(shinythemes)
 
 Child_Mortality_data_set_Cleaned <- read_excel("C:/Users/admin/Desktop/Masters-MSC/APHREA/Health Data Sets/Child Mortality data set- Cleaned.xlsx")
 
-# Renaming the columns
-
-colnames(Child_Mortality_data_set_Cleaned)=c("Continent","Country","Year","Age_Group","Cause_of_Death","Prevalence")
-
-View(Child_Mortality_data_set_Cleaned)
-str(Child_Mortality_data_set_Cleaned)
-summary(Child_Mortality_data_set_Cleaned)
-
-# Checking for any missing values
-## In totality
-## Per Column
-sum(is.na(Child_Mortality_data_set_Cleaned))
-colSums(is.na(Child_Mortality_data_set_Cleaned))
-names(Child_Mortality_data_set_Cleaned$Continent)
-
-boxplot(Prevalence ~ Continent, data = Child_Mortality_data_set_Cleaned,
-        main = "Prevalence by Continent",
-        ylab = "Prevalence",
-        xlab = "Continent",
-        col = "lightblue")
-
-# To check if prevalence significantly differs across continents
-
-Anova_mortality= aov(Prevalence ~ Continent, data = Child_Mortality_data_set_Cleaned)
-summary(Anova_mortality)
-
 # Shiny App
 
 ui <- fluidPage(
